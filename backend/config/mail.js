acb982594a1a4955fbc5b6626f99b86768562662
Abhijeet Narsale <abhijeetnarsale14082004@gmail.com>
2026-07-15 12:26:50 +0530

    Force IPv4 for Gmail SMTP

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,13 +1,22 @@
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   requireTLS: true,
+
+  // Force IPv4
+  family: 4,
+
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
 });
 
 module.exports = transporter;
