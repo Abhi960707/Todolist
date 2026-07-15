0291162ec74f55af111793e5177cdfd06dcb94d5
Abhijeet Narsale <abhijeetnarsale14082004@gmail.com>
2026-07-15 12:32:17 +0530

    Update Gmail SMTP configuration

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -6,17 +6,18 @@ const transporter = nodemailer.createTransport({
   secure: false,
   requireTLS: true,
 
-  // Force IPv4
-  family: 4,
-
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
   },
 
+  family: 4,
+
   connectionTimeout: 10000,
   greetingTimeout: 10000,
-  socketTimeout: 15000,
+  socketTimeout: 10000,
+
+  dnsTimeout: 10000,
 });
 
 module.exports = transporter;
